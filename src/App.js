1a447fe98f0c3203c7a7b3d51d5aa4b251044c5a
Moon-Seng <moonseng.dev@gamil.com>
2022-06-22 14:21:50 +0630

    Add : Footer Carousel Component to Master

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import Nav from "./Components/NavComponent";
 import Header from './Components/HeaderComponent';
+import FooterCarousel from './Components/FooterCarousel'
+import ContentComponent from "./Components/ContentComponent";
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Header />  
+      <ContentComponent />
+      <FooterCarousel />
+      
     </div>
   );
 }

7cd68b4be8541bf2645525b6dece52c21e293182
Moon-Seng <moonseng.dev@gamil.com>
2022-06-28 12:54:55 +0630

    Modified : fixed sass error

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from "react";
 import Nav from "./core/components/nav/nav";
 import Header from "./core/components/header/header";
 import FooterCarousel from "./core/components/footer-carousel/footer-carousel";
-import Content from "./core/components/content/content";
 import PageFooter from "./core/components/page-footer/page-footer";
 import Copyright from "./core/components/copy-right/copy-right";
 import ScrollButton from "./core/components/scroll-button/scroll-button";
+import TeamSingle from "./app/pages/team-single/team-single";
 
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
     <div className="App">
       <Nav />
       <Header />
-      <Content />
+      <TeamSingle />
       <FooterCarousel />
       <PageFooter />
       <Copyright />
---
+++ src/app/pages/team-single/team-single.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import TeamSingleStyle from './team-single.module.sass'
+
+function teamSingle() {
+  return (
+    <div className={TeamSingleStyle.header}>Hello World</div>
+  )
+}
+
+export default teamSingle

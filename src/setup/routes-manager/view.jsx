6e9cbb6bb991121a3ced762d9cf307714798ecfb
Moon-Seng <moonseng.dev@gamil.com>
2022-06-30 14:33:49 +0630

    Modified : Create contact route

--- src/setup/routes-manager/view.jsx
+++ src/setup/routes-manager/view.jsx
@@ -3,12 +3,14 @@ import { Routes , Route } from 'react-router-dom'
 import Home from '../../app/pages/home/home'
 import About from '../../app/pages/about/about'
 import Error from '../../app/pages/error/error'
+import Contact from '../../app/pages/contact/contact'
 
 function View() {
   return (
     <Routes>
         <Route index element={<Home />} ></Route>
         <Route path='/about' element={<About />} ></Route>
+        <Route path='/contact' element={<Contact />} ></Route>
         <Route path='*' element={<Error />}></Route>
     </Routes>
   )

1b3615f1b9c78869d91fc6f4ef38b96b25103b62
heinwaiyanhtet <heinwaiyanhtet2020@gmail.com>
2022-07-01 21:11:02 +0630

    team single with personal experience

--- src/app/pages/team-single/team-single.jsx
+++ src/app/pages/team-single/team-single.jsx
@@ -72,6 +72,41 @@ function teamSingle() {
 							</table>
         </div>
       </div>
+
+
+
+      <div className={TeamSingleStyle.PersonalExperience}>
+      <div className={TeamSingleStyle.personalExperienceContainer}>
+
+          <div className={TeamSingleStyle.content}>
+              <h5>Personal Experience</h5>
+              <p>Doloremque quam. Exercitation totam or cupidatat culpa but autem. In labore so nostrud asper natur officia magna, so quasi. Sit nequeporro dolor Iure. Pellentesque lorem mauris, vehicula vel nulla nec.</p>
+          </div>
+          
+          <div className={TeamSingleStyle.contentpart2}>
+              <div className={TeamSingleStyle.left}>
+                  <p> INTERNET CONNECTION<span>93%</span></p>
+                  <div  className={`${TeamSingleStyle.progress} ${TeamSingleStyle.pg1}`}></div>
+
+                  <p> IPTV INSTALLATION<span>90%</span></p>
+                  <div className={`${TeamSingleStyle.progress} ${TeamSingleStyle.pg2}`}></div>
+
+                  <p> VOIP SETUP<span>98%</span></p>
+                  <div  className={`${TeamSingleStyle.progress} ${TeamSingleStyle.pg3}`}></div>
+
+                  <p> SALES MANAGING<span>93%</span></p>
+                  <div className={`${TeamSingleStyle.progress} ${TeamSingleStyle.pg4}`}></div>
+              </div>
+
+              <div className={TeamSingleStyle.right}>
+                  <p>Rem explicabo proident, and culpa and proident for iste and modi. Tot am aliqua but exercitation. Tiam ut bibendum leo.</p>
+                  <p>Lorem and ex, and incidunt. Ad cons equuntur. Laboriosam conse quatur ad. Ipsa perspiciatis, but laboris and omnis. Excepteur quia totam or cupidatat.</p>
+              </div>
+            
+          </div>
+        
+      </div>
+    </div>
     </div>
   )
 }
